92689303f2b8d3e58cc84438bd75ddecb8bcede8
Wamaitha <wamaithaweru19@gmail.com>
2022-04-17 08:16:57 +0300

    adding Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './profile-component/display/display.component';
+import { ContentComponent } from './content-component/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent
+    DisplayComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
