f26ccd88c71cf19a9d59b27c0c68cc121d1d7b64
Wamaitha <wamaithaweru19@gmail.com>
2022-04-17 09:36:00 +0300

    adds a service 'user-name'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ContentComponent } from '../app/component/content/content-component/content/content.component';
+import { DisplayComponent } from '../app/component/display/display/display.component';
+const routes: Routes = [ 
+  { path: 'display', component: DisplayComponent},
+  {path: 'content' , component: ContentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './profile-component/display/display.component';
 import { ContentComponent } from './content-component/content/content.component';
+import { NavComponent } from './navbar-component/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DisplayComponent,
-    ContentComponent
+    ContentComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
